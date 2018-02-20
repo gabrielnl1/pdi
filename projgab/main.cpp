9ba33eb96361613d8b30f6ef5dc37576ab17279b
Gabriel <gabrielnl.obs@gmail.com>
2018-02-20 11:10:40 -0300

    gg2
    Signed-off-by: Gabriel <gabrielnl.obs@gmail.com>

--- projgab/main.cpp
+++ projgab/main.cpp
@@ -53,6 +53,65 @@ int main(int argc, char *argv[])
     */
 
     /*
+     *
+     * ADICIONAR RUIDO DISTRIBUIÇÃO NORMAL MEDIA 127 DESVIO 50 e CALCULAR HISTOGRAMA COLORIDO
+     *
+     *  Mat src(500, 500, CV_8UC3);
+    randn(src, 127, 50);
+
+    Mat dst;
+
+    /// Separate the image in 3 places ( B, G and R )
+    vector<Mat> bgr_planes;
+    split( src, bgr_planes );
+
+    /// Establish the number of bins
+    int histSize = 256;
+
+    /// Set the ranges ( for B,G,R) )
+    float range[] = { 0, 256 } ;
+    const float* histRange = { range };
+
+    bool uniform = true; bool accumulate = false;
+
+    Mat b_hist, g_hist, r_hist;
+
+    /// Compute the histograms:
+    calcHist( &bgr_planes[0], 1, 0, Mat(), b_hist, 1, &histSize, &histRange, uniform, accumulate );
+    calcHist( &bgr_planes[1], 1, 0, Mat(), g_hist, 1, &histSize, &histRange, uniform, accumulate );
+    calcHist( &bgr_planes[2], 1, 0, Mat(), r_hist, 1, &histSize, &histRange, uniform, accumulate );
+
+    // Draw the histograms for B, G and R
+    int hist_w = 512; int hist_h = 400;
+    int bin_w = cvRound( (double) hist_w/histSize );
+
+    Mat histImage( hist_h, hist_w, CV_8UC3, Scalar( 0,0,0) );
+
+    /// Normalize the result to [ 0, histImage.rows ]
+    normalize(b_hist, b_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
+    normalize(g_hist, g_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
+    normalize(r_hist, r_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
+
+    /// Draw for each channel
+    for( int i = 1; i < histSize; i++ )
+    {
+        line( histImage, Point( bin_w*(i-1), hist_h - cvRound(b_hist.at<float>(i-1)) ) ,
+                         Point( bin_w*(i), hist_h - cvRound(b_hist.at<float>(i)) ),
+                         Scalar( 255, 0, 0), 2, 8, 0  );
+        line( histImage, Point( bin_w*(i-1), hist_h - cvRound(g_hist.at<float>(i-1)) ) ,
+                         Point( bin_w*(i), hist_h - cvRound(g_hist.at<float>(i)) ),
+                         Scalar( 0, 255, 0), 2, 8, 0  );
+        line( histImage, Point( bin_w*(i-1), hist_h - cvRound(r_hist.at<float>(i-1)) ) ,
+                         Point( bin_w*(i), hist_h - cvRound(r_hist.at<float>(i)) ),
+                         Scalar( 0, 0, 255), 2, 8, 0  );
+    }
+
+    /// Display
+    namedWindow("calcHist Demo", CV_WINDOW_AUTOSIZE );
+    imshow("calcHist Demo", histImage );
+    namedWindow("img original", WINDOW_KEEPRATIO);
+    imshow("img original", src );
+    waitKey(0);
 
     // Preencher uma matriz 500x500 com números aleatórios normalmente distribuídos e somar com outra imagem e calcular o histograma da imagem com números aleatórios, tons de cinza
 
@@ -173,28 +232,64 @@ int main(int argc, char *argv[])
     }
     */
 
-    Mat img = imread("spectrum.tif", IMREAD_GRAYSCALE);
-    Mat img2;
-    img2.create(img.rows, img.cols, CV_64F);
-    double c= 1;
-    for (int x = 0; x < img.cols; x++) {
-        for (int y = 0; y < img.rows; y++) {
-            Scalar intensity = img.at<uchar>(y,x);
-            double intensity_new = c* log (1+(double)intensity.val[0]);
-            img2.at<uchar>(y,x) = intensity_new;
-        }
-    }
-    normalize(img2,img2,1,0,NORM_MINMAX);
-    //para normalizar uma imagem que tem valores negativos, a normalização é I-min(I)/max(I)-min(I) ,
-    //onde I é a imagem, min(I) é o menor valor dos pixels da imagem, e max(I) é o valor máximo
-    img2 = 255*img2;
-    img2.convertTo(img2, CV_8U);
+    // utilizar transformação logaritmica ou exponencial para realçar os detalhes de uma imagem
+    /*
 
+    Mat img = imread("spectrum.tif", IMREAD_GRAYSCALE);
+    int n = 50;
     namedWindow("img",WINDOW_KEEPRATIO);
     namedWindow("img2",WINDOW_KEEPRATIO);
-    imshow("img", img);
-    imshow("img2",img2);
-    waitKey(0);
+    createTrackbar("n", "img2", &n, 100, 0,0);
+    double c = 1;
+    for (;;) {
+        //Mat img2 = Mat::zeros(img.rows,img.cols, CV_64F);
+        Mat img2;
+        img2.create(img.rows, img.cols, CV_64F);
+        for (int x = 0; x < img.cols; x++) {
+            for (int y = 0; y < img.rows; y++) {
+                Scalar intensity = img.at<uchar>(y,x);
+                double intensity_new = pow((double)intensity.val[0],25*(double)((n)/1000.0));
+                //double intensity_new = c* log (1+(double)intensity.val[0]);
+                img2.at<double>(y,x) = intensity_new;
+            }
+        }
+        normalize(img2,img2,1,0,NORM_MINMAX);
+        //para normalizar uma imagem que tem valores negativos, a normalização é I-min(I)/max(I)-min(I) ,
+        //onde I é a imagem, min(I) é o menor valor dos pixels da imagem, e max(I) é o valor máximo
+        img2 = 255*img2;
+        img2.convertTo(img2, CV_8U);
+        imshow("img", img);
+        imshow("img2",img2);
+        if((char)waitKey(1) == 'q') break;
+    }
+    */
+
+    Mat img = imread("kidney.tif", IMREAD_GRAYSCALE);
+    int thresh1 = 127;
+    int thresh2 = 240;
+    namedWindow("img",WINDOW_KEEPRATIO);
+    namedWindow("img2",WINDOW_KEEPRATIO);
+    createTrackbar("thresh1", "img2", &thresh1, 255, 0,0);
+    createTrackbar("thresh2", "img2", &thresh2, 255, 0,0);
+    for (;;) {
+        //Mat img2 = Mat::zeros(img.rows,img.cols, CV_8U);
+        Mat img2;
+        img2.create(img.rows, img.cols, CV_8U);
+        for (int x = 0; x < img.cols; x++) {
+            for (int y = 0; y < img.rows; y++) {
+                Scalar intensity = img.at<uchar>(y,x);
+                if (intensity.val[0] > thresh1 && intensity.val[0] < thresh2 ) {
+                    img2.at<uchar>(y,x) = 255;
+                }
+                else {
+                    img2.at<uchar>(y,x) = 0;
+                }
+            }
+        }
+        imshow("img", img);
+        imshow("img2",img2);
+        if((char)waitKey(1) == 'q') break;
+    }
 
     return a.exec();
 }
